710d8bc2e3a844caa01060ffde714e39e1d9607c
kfbustam <karoljosefb@gmail.com>
2022-03-20 22:21:04 -0700

    make etsy logo link to home page

--- src/client/nav/EtsyLogo.jsx
+++ src/client/nav/EtsyLogo.jsx
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import etsyLogo from '../../../public/Etsy_logo.svg.png';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { changePageView as changePageViewAction } from '../store/actions/pageAction';
+import { PAGES } from '../store/actions/actionTypes';
 
 const iconStyle = {
   height: 30,
   width: 60,
-  margin: 'auto 6px',
 };
 
+const rootStyle = {
+  margin: 'auto 6px',
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
 class EtsyLogo extends Component {
   render() {
+    const {changePageView} = this.props;
+
     return (
-      <img style={iconStyle} src={etsyLogo} alt="Logo" />
+      <div style={rootStyle} onClick={() => changePageView(PAGES.HOME)} onKeyPress={() => changePageView(PAGES.HOME)} role="button" tabIndex="-1" >
+        <img style={iconStyle} src={etsyLogo} alt="Logo" />
+      </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+  changePageView: userLoginDetails => dispatch(changePageViewAction(userLoginDetails))
+});
 
 EtsyLogo.defaultProps = {
 };
@@ -23,4 +40,6 @@ EtsyLogo.defaultProps = {
 EtsyLogo.propTypes = {
 };
 
-export default EtsyLogo;
+
+export default connect(mapStateToProps, mapDispatchToProps)(EtsyLogo);
+
